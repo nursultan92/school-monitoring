cfd9c621ca8a0118c466e1c9ed8bcc9da0e0761a
Nursultan <nursultan2010@gmail.com>
2014-04-02 15:33:01 +0400

    StudentController

---
+++ src/Application/SchoolBundle/Controller/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 02.04.14
+ * Time: 15:32
+ */
+
+namespace Application\SchoolBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StudentController extends Controller
+{
+
+} 
